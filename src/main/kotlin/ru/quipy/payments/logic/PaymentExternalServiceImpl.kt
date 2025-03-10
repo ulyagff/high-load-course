f6989358e320cab5c0c699b23156492fe6702d37
Anna <snusha.spb@gmail.com>
2025-03-10 19:15:43 +0300

    initial commit

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -1,6 +1,5 @@
 package ru.quipy.payments.logic
 
-
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.util.concurrent.Semaphore
